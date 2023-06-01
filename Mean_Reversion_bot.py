95405d4631d4a76d2a2171f435906ed22f38416a
Your NamePrimo <ullyssis.patorito23@gmail.com>
2023-06-01 12:04:11 +0800

    modified:   Mean_Reversion_bot.py modified:   bot template.py

--- Mean_Reversion_bot.py
+++ Mean_Reversion_bot.py
@@ -4,6 +4,9 @@ import credentials
 import talib as ta
 import numpy as np
 import datetime
+from pytz import timezone
+import schedule
+import time
 
 exchange = ccxt.binance({
     'enableRateLimit': True,
@@ -11,58 +14,73 @@ exchange = ccxt.binance({
     'secret': credentials.api_secret,
 })
 
-#Variables
-
-symbols = ['BTC/USDT','ETH/USDT','ADA/USDT','SOL/USDT']
+# Variables
+symbols = ['SOL/USDT', 'ADA/USDT','BNB/USDT','LTC/USDT']  # Updated variable name from symbolS to symbols
 trend = '1D'
 exec_tf = '15m'
 rsi_period = 2
-rsi_overbought = 80
-rsi_oversold = 28
+rsi_overbought = 90
+rsi_oversold = 10
 sma_period = 200
 max_open_position = 3
-per_trade_amount = 0.1
-start_trade = 15
-end_trade = 3
+per_trade_amount = 1.0
+per_trade_sell = 1.0
+start_trade = 21  # New York session start time (9:00 PM UTC)
+end_trade = 11  # London session end time (11:00 AM UTC)
+
+in_position = False
+
+def MeanReversionBot():
+    global in_position
+
+    # # Get the current UTC time
+    # utc_now = datetime.datetime.now(timezone('UTC'))
+
+    # # Convert UTC time to Philippines time
+    # ph_timezone = timezone('Asia/Manila')
+    # ph_time = utc_now.astimezone(ph_timezone).time()
+
+    # # Check if it's within the trading window
+    # if start_trade <= ph_time.hour <= end_trade:
+    print('Scanning')
+    for symbol in symbols:
+        # Fetch OHLCV data
+        daily_bars = exchange.fetch_ohlcv(symbol, timeframe='1d', limit=sma_period)
+        daily_close_price = [bar[4] for bar in daily_bars]
+        daily_sma = ta.SMA(np.array(daily_close_price), sma_period)
+
+        # Check daily trend
+        if daily_close_price[-1] > daily_sma[-1]:
+            print(f'{symbol} is bullish, waiting for m15_rsi confirmation')
+            m15_bars = exchange.fetch_ohlcv(symbol, timeframe='15m', limit=sma_period)
+            m15_close_price = [bar[4] for bar in m15_bars]
+            m15_rsi = ta.RSI(np.array(m15_close_price), rsi_period)
+
+            if m15_rsi[-1] < rsi_oversold:
+                if not in_position:
+                    balance = exchange.fetch_balance()['total']['USDT']
+                    trade_amount = balance * per_trade_amount
+                    buy = exchange.create_market_buy_order(symbol, trade_amount)
+                    print(buy)
+                    in_position = True
+                else:
+                    print("Already in a position")
+
+            elif m15_rsi[-1] > rsi_overbought:
+                if in_position:
+                    coin_balance = exchange.fetch_balance()['total'][symbol.split('/')[0]]
+                    sell_amount = coin_balance * per_trade_sell
+                    sell = exchange.create_market_sell_order(symbol, sell_amount)
+                    print(sell)
+                    in_position = False
+                else:
+                    print('No open positions yet')
+
+def run_bot():
+    MeanReversionBot()
+
+schedule.every(10).seconds.do(run_bot)
 
 while True:
-    # Check if it's within the trading window
-    current_hour = datetime.datetime.now().hour
-    if current_hour >= start_trade and current_hour <= end_trade:
- 
-        for symbol in symbols:
-            # Fetch OHLCV data
-            daily_bars = exchange.fetch_ohlcv(symbol, timeframe='1d', limit=sma_period)
-            daily_close_price = [bar[4] for bar in daily_bars]
-            daily_sma = ta.SMA(np.array(daily_close_price), sma_period)
-            print(f'Daily SMA = {symbol} | {daily_sma[-1]}')
-
-            #Check daily trend
-            if daily_close_price[-1] > daily_sma[-1]:
-                m15_bars = exchange.fetch_ohlcv(symbol, timeframe='15m', limit=sma_period)
-                m15_close_price = [bar[4] for bar in m15_bars]
-                m15_sma = ta.SMA(np.array(m15_close_price),sma_period)
-
-                if m15_close_price > m15_sma:
-                    m15_rsi = ta.RSI(np.array(m15_close_price), rsi_period)
-                    print(f'm15_RSI = {symbol} | {m15_rsi[-1]}')
-                    last_bar = m15_bars
-
-                    if m15_rsi[-1] < rsi_oversold:
-                        open_positions = sum(len(exchange.fetch_open_orders(symbol)) for symbol in symbols)
-                        if open_positions < max_open_position:
-                            balance = exchange.fetch_balance()['total']['USDT']
-                            trade_amount = balance * per_trade_amount
-                            buy = exchange.create_market_buy_order(symbol, trade_amount)
-                            print(f'Entered Trade: {symbol}')
-
-                    elif m15_rsi[-1] > rsi_overbought:
-                        open_positions = sum(len(exchange.fetch_open_orders(symbol)) for symbol in symbols)
-                        if open_positions > 0:
-                            open_orders = exchange.fetch_open_orders(symbol)
-                            for order in open_orders:
-                                sell = exchange.cancel_order(order['id'])
-                                print(f'Exited Trade: {symbol}')
-
-    else:
-        break
+    schedule.run_pending()
+    time.sleep(1)
--- bot template.py
+++ bot template.py
@@ -1,77 +1,86 @@
-# Constants
-SYMBOLS = ['BTC/USDT', 'ETH/USDT', 'ADA/USDT', 'SOL/USDT']
-TIMEFRAME = '15m'
-DAILY_TIMEFRAME = '1d'  # Daily timeframe
-SMA_PERIOD = 200
-RSI_PERIOD = 2
-RSI_OVERBOUGHT = 80
-RSI_OVERSOLD = 28
-TRADE_WINDOW_START = 8
-TRADE_WINDOW_END = 20
-MAX_POSITIONS = 3
-TRADE_AMOUNT_PERCENTAGE = 0.1  # 10% of account balance per trade
+import ccxt
+import pandas as pd
+import credentials
+import talib as ta
+import numpy as np
+import datetime
+from pytz import timezone
+import schedule
+import time
 
-async def main():
-    # Create Binance exchange instance
-    exchange = ccxt.binance({
-        'apiKey': 'UMjRag1HfZFAaL5FgU4sfBS5R4WUOXLKPVOspmjZgsNyxk2Tk16iIumbtH2oVhub',
-        'secret': 'tGhc7g0FyHZyOOwmmP9q0DCOy8dQA9MXZJYiGS5mRQPQ5awJs8BMkVuHjuRvLHyU',
-    })
-    
-    # Start trading loop
-    while True:
-        # Check if it's within the trading window
-        current_hour = datetime.datetime.now().hour
-        if current_hour >= TRADE_WINDOW_START and current_hour < TRADE_WINDOW_END:
-            # Iterate over symbols
-            for symbol in SYMBOLS:
-                # Fetch daily data
-                daily_bars = await exchange.fetch_ohlcv(symbol, timeframe=DAILY_TIMEFRAME, limit=SMA_PERIOD)
-                daily_close_prices = [bar[4] for bar in daily_bars]  # Daily closing prices
-                
-                # Calculate daily SMA
-                daily_sma = talib.SMA(np.array(daily_close_prices), SMA_PERIOD)
-                
-                # Check daily trend
-                if daily_close_prices[-1] > daily_sma[-1]:
-                    # Fetch 15-minute data
-                    bars = await exchange.fetch_ohlcv(symbol, timeframe=TIMEFRAME, limit=SMA_PERIOD)
-                    close_prices = [bar[4] for bar in bars]  # Closing prices
-                    
-                    # Calculate SMA
-                    sma = talib.SMA(np.array(close_prices), SMA_PERIOD)
-                    
-                    # Fetch current RSI value
-                    last_bar = await exchange.fetch_ohlcv(symbol, timeframe=TIMEFRAME, limit=1)
-                    current_price = last_bar[0][4]  # Current closing price
-                    rsi = talib.RSI(np.array(close_prices), RSI_PERIOD)
-                    current_rsi = rsi[-1]
-                    
-                    # Check trading conditions
-                    if current_price > sma[-1] and current_rsi < RSI_OVERSOLD:
-                        # Check open position limit
-                        open_positions = len(exchange.fetch_open_orders(symbol))
-                        if open_positions < MAX_POSITIONS:
-                            # Get account balance
-                            balance = exchange.fetch_balance()['total']['USDT']
-                            # Calculate trade amount
-                            trade_amount = balance * TRADE_AMOUNT_PERCENTAGE
-                            price = current_price  # Current market price
-                            await exchange.create_market_buy_order(symbol, trade_amount)
-                            print(f"Entered trade: {symbol}")
-                    
-                    # Check for exit condition
-                    if current_rsi > RSI_OVERBOUGHT:
-                        open_positions = len(exchange.fetch_open_orders(symbol))
-                        if open_positions > 0:
-                            # Exit trade
-                            open_orders = exchange.fetch_open_orders(symbol)
-                            for order in open_orders:
-                                await exchange.cancel_order(order['id'])
-                                print(f"Exited trade: {symbol}")
-        
-        # Sleep for 1 minute before next iteration
-        await asyncio.sleep(60)
+exchange = ccxt.binance({
+    'enableRateLimit': True,
+    'apiKey': credentials.api_key,
+    'secret': credentials.api_secret,
+})
 
-# Run the trading bot
-asyncio.run(main())
+# Variables
+symbols = ['SOL/USDT', 'ADA/USDT','BNB/USDT','LTC/USDT']  # Updated variable name from symbolS to symbols
+trend = '1D'
+exec_tf = '15m'
+rsi_period = 2
+rsi_overbought = 90
+rsi_oversold = 10
+sma_period = 200
+max_open_position = 3
+per_trade_amount = 1.0
+per_trade_sell = 1.0
+start_trade = 21  # New York session start time (9:00 PM UTC)
+end_trade = 11  # London session end time (11:00 AM UTC)
+
+in_position = False
+
+def MeanReversionBot():
+    global in_position
+
+    # # Get the current UTC time
+    # utc_now = datetime.datetime.now(timezone('UTC'))
+
+    # # Convert UTC time to Philippines time
+    # ph_timezone = timezone('Asia/Manila')
+    # ph_time = utc_now.astimezone(ph_timezone).time()
+
+    # # Check if it's within the trading window
+    # if start_trade <= ph_time.hour <= end_trade:
+    print('Scanning')
+    for symbol in symbols:
+        # Fetch OHLCV data
+        daily_bars = exchange.fetch_ohlcv(symbol, timeframe='1d', limit=sma_period)
+        daily_close_price = [bar[4] for bar in daily_bars]
+        daily_sma = ta.SMA(np.array(daily_close_price), sma_period)
+
+        # Check daily trend
+        if daily_close_price[-1] > daily_sma[-1]:
+            print(f'{symbol} is bullish, waiting for m15_rsi confirmation')
+            m15_bars = exchange.fetch_ohlcv(symbol, timeframe='15m', limit=sma_period)
+            m15_close_price = [bar[4] for bar in m15_bars]
+            m15_rsi = ta.RSI(np.array(m15_close_price), rsi_period)
+
+            if m15_rsi[-1] < rsi_oversold:
+                if not in_position:
+                    balance = exchange.fetch_balance()['total']['USDT']
+                    trade_amount = balance * per_trade_amount
+                    buy = exchange.create_market_buy_order(symbol, trade_amount)
+                    print(buy)
+                    in_position = True
+                else:
+                    print("Already in a position")
+
+            elif m15_rsi[-1] > rsi_overbought:
+                if in_position:
+                    coin_balance = exchange.fetch_balance()['total'][symbol.split('/')[0]]
+                    sell_amount = coin_balance * per_trade_sell
+                    sell = exchange.create_market_sell_order(symbol, sell_amount)
+                    print(sell)
+                    in_position = False
+                else:
+                    print('No open positions yet')
+
+def run_bot():
+    MeanReversionBot()
+
+schedule.every(10).seconds.do(run_bot)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
